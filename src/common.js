2b8236897e8ddcaad8af0fa0c5619db321d57d8f
Ludy87 <Ludy87@users.noreply.github.com>
2026-08-27 22:14:59 +0200

    cache: remove 'use-default-token' input and simplify token retrieval logic

--- src/common.js
+++ src/common.js
@@ -13,10 +13,7 @@ function input(name, defaultValue = '') {
 }
 
 function token() {
-  const configured = input('token');
-  if (configured) return configured;
-  if (String(input('use-default-token', 'true')).toLowerCase() !== 'true') return '';
-  return process.env.GITHUB_TOKEN || process.env.ACTIONS_RUNTIME_TOKEN;
+  return input('token') || process.env.GITHUB_TOKEN || process.env.ACTIONS_RUNTIME_TOKEN;
 }
 
 function log(message) {
